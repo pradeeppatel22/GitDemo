e9e98fda9fbd6b83e22e69ba04614d19155494e2
Pradeep <pradeep.patel22@outlook.com>
2023-03-09 18:07:30 +0530

    changes made by Y

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -17,9 +17,10 @@ public class RESTAPITest {
 		System.out.println("postJira5");
 		System.out.println("postJira6");
 		System.out.println("postJira7");
-		
-		
-		
+		System.out.println("postJira8");
+		System.out.println("postJira9");
+		System.out.println("postJira10");
+		System.out.println("postJira11);
 		
 		
 		
